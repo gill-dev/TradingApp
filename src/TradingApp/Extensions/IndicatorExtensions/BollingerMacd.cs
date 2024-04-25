5d56f294c79619222a47c863ba34e5dc67175889
gillsky <gillian.wolmarans@sky.uk>
2024-04-25 14:33:00 +0100

    New Strategy

---
+++ src/TradingApp/Extensions/IndicatorExtensions/BollingerMacd.cs
@@ -0,0 +1,57 @@
+﻿using TradingApp.Models.DataTransferObjects;
+using TradingApp.Models.Enums;
+using TradingApp.Models.Indicators;
+
+namespace TradingApp.Extensions.IndicatorExtensions
+{
+    public static partial class Indicator
+    {
+        public static IndicatorResult[] CalcBollingerMacdEma(this Candle[] candles, int bbWindow = 12, double stdDev = 2,
+            int emaWindow = 100, double maxSpread = 0.0004, double minGain = 0.0006, double riskReward = 1.5)
+        {
+            var bollingerBands = candles.CalcBollingerBands(bbWindow, stdDev);
+            var macdResults = candles.CalcMacd().ToArray();
+            var prices = candles.Select(c => c.Mid_C).ToArray();
+
+            var emaResult = prices.CalcEma(emaWindow).ToArray();
+
+            var length = candles.Length;
+            var result = new IndicatorResult[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = new IndicatorResult
+                {
+                    Candle = candles[i]
+                };
+
+                var currentMacd = macdResults[i];
+                var currentEma = emaResult[i];
+                var currentBollinger = bollingerBands[i];
+
+                var macDelta = currentMacd.Macd - currentMacd.SignalLine;
+                var macDeltaPrev = i == 0 ? 0 : macdResults[i - 1].Macd - macdResults[i - 1].SignalLine;
+
+                int direction = macDelta > 0 && macDeltaPrev < 0 ? 1 :
+                    macDelta < 0 && macDeltaPrev > 0 ? -1 : 0;
+
+                result[i].Gain = Math.Abs(candles[i].Mid_C - currentEma);
+
+                result[i].Signal = direction switch
+                {
+                    1 when candles[i].Mid_L > currentEma && candles[i].Mid_C > currentBollinger.LowerBand && candles[i].Spread <= maxSpread && result[i].Gain >= minGain => Signal.Buy,
+                    -1 when candles[i].Mid_H < currentEma && candles[i].Mid_C < currentBollinger.UpperBand && candles[i].Spread <= maxSpread && result[i].Gain >= minGain => Signal.Sell,
+                    _ => Signal.None
+                };
+
+                result[i].TakeProfit = candles[i].CalcTakeProfit(result[i]);
+                result[i].StopLoss = candles[i].CalcStopLoss(result[i], riskReward);
+                result[i].Loss = Math.Abs(candles[i].Mid_C - result[i].StopLoss);
+            }
+
+            return result;
+        }
+
+
+    }
+}
--- src/TradingApp/Services/TradeManager.cs
+++ src/TradingApp/Services/TradeManager.cs
@@ -78,8 +78,7 @@ public class TradeManager : BackgroundService
             return;
         }
 
-        var calcResult = candles.CalcRsiBollingerBands(settings.Integers[0], settings.Integers[1], settings.Doubles[0],
-            settings.MaxSpread, settings.MinGain, settings.MinVolume, settings.RiskReward, settings.Doubles[1], settings.Doubles[2]).Last();
+        var calcResult = candles.CalcBollingerMacdEma().Last();
 
         if (calcResult.Signal != Signal.None && await SignalFollowsTrend(settings, calcResult.Signal))
         {
