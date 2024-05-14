9e0f648348c98a6ce4f65648b0ac3e317d012710
gillsky <gillian.wolmarans@sky.uk>
2024-05-14 15:48:24 +0100

    try new strategy

--- src/Trading.Bot/Services/TradeManager.cs
+++ src/Trading.Bot/Services/TradeManager.cs
@@ -82,7 +82,7 @@ public class TradeManager : BackgroundService
         //    await TryPlaceTrade(settings, calcResult);
         //    return;
         //}
-        var calcResult = candles.CalcBollingerBandsEma(settings.Integers[0], settings.Integers[1],
+        var calcResult = candles.CalcBollingerBandsEmaMacd(settings.Integers[0], settings.Integers[1],
             settings.Doubles[0], settings.MaxSpread, settings.MinGain, settings.MinVolume, settings.RiskReward).Last();
 
         if (calcResult.Signal != Signal.None && await SignalFollowsTrend(settings, calcResult.Signal))
