d1549fa147d58282244c7a0f50134ce1d6671cdf
gillsky <gillian.wolmarans@sky.uk>
2024-05-21 16:11:59 +0100

    update trade

--- src/Trading.Bot/Extensions/IndicatorExtensions/MacdEma.cs
+++ src/Trading.Bot/Extensions/IndicatorExtensions/MacdEma.cs
@@ -5,11 +5,12 @@
         public static IndicatorResult[] CalcMacdEma(this Candle[] candles, int bbWindow = 20, int emaWindow = 100,
             double stdDev = 2, double maxSpread = 0.0004, double minGain = 0.0006, int minVolume = 100, double riskReward = 1.5)
         {
-            var macd = candles.CalcMacd().ToArray();
+            var macd = candles.CalcMacd();
+
             var prices = candles.Select(c => c.Mid_C).ToArray();
 
-            var emaShort = prices.CalcEma(15).ToArray();
-            var emaLong = prices.CalcEma(60).ToArray();
+            var emaShort = prices.CalcEma(13).ToArray();
+            var emaLong = prices.CalcEma(55).ToArray();
 
             var length = candles.Length;
             var result = new IndicatorResult[length];
@@ -32,24 +33,35 @@
 
                 result[i].Gain = Math.Abs(candles[i].Mid_C - emaShort[i]);
 
+                result[i].Signal = direction switch
+                {
+                    1 when emaShort[i] > emaLong[i] && 
+                           candles[i].Spread <= maxSpread &&
+                           result[i].Gain >= minGain => Signal.Buy,
+                    -1 when emaShort[i] < emaLong[i] &&
+                            candles[i].Spread <= maxSpread &&
+                            result[i].Gain >= minGain => Signal.Sell,
+                    _ => Signal.None
+                };
+
                 // Check criteria for long trades
-                if (emaShort[i] > emaLong[i] && macd[i].Macd < 0)
-                {
-                    result[i].Signal = direction == 1 && candles[i].Spread <= maxSpread && result[i].Gain >= minGain
-                        ? Signal.Buy
-                        : Signal.None;
-                }
-                // Check criteria for short trades
-                else if (emaShort[i] < emaLong[i] && macd[i].Macd > 0 )
-                {
-                    result[i].Signal = direction == -1 && candles[i].Spread <= maxSpread && result[i].Gain >= minGain
-                        ? Signal.Sell
-                        : Signal.None;
-                }
-                else
-                {
-                    result[i].Signal = Signal.None;
-                }
+                //if (emaShort[i] > emaLong[i] && macd[i].Macd < 0)
+                //{
+                //    result[i].Signal = direction == 1 && candles[i].Spread <= maxSpread && result[i].Gain >= minGain
+                //        ? Signal.Buy
+                //        : Signal.None;
+                //}
+                //// Check criteria for short trades
+                //else if (emaShort[i] < emaLong[i] && macd[i].Macd > 0 )
+                //{
+                //    result[i].Signal = direction == -1 && candles[i].Spread <= maxSpread && result[i].Gain >= minGain
+                //        ? Signal.Sell
+                //        : Signal.None;
+                //}
+                //else
+                //{
+                //    result[i].Signal = Signal.None;
+                //}
 
                 result[i].TakeProfit = candles[i].CalcTakeProfit(result[i], riskReward);
                 result[i].StopLoss = candles[i].CalcStopLoss(result[i]);
